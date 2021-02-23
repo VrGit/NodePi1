5ad01724486032e1844f3e7633f82b4424a2449e
Thierry <thierry.leydier@gmail.com>
2021-02-23 16:51:12 +0100

    xx

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.static('public')) ;
 // app.use('/static', express.static(path.join(__dirname + '/public')));
 
 // app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname + '/public/index.html'));
+//      res.sendFile(path.join(__dirname + '/public/index.html'));
 // });
 
 app.get ('/params', function (req,res) {
